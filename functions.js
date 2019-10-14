9d7bd364ed86121414f136a157a09b55b673a92c
Yunior <yuniorzen@qq.com>
2019-10-14 16:10:16 +0800

    函数节流限制频次

--- functions.js
+++ functions.js
@@ -1,3 +1,42 @@
+
+/**
+ * 函数节流限制频次,每time毫秒只会执行一次callback函数
+ * @method throttleFn
+ * 
+ * @params
+ * delay {Number} 毫秒数
+ * callback   {Function} 待执行的函数
+ * @returns
+ * 
+ * @author Yunior
+ * */
+export const throttleFn=(delay, callback)=>{
+    let timeoutID;
+    let lastExec = 0;
+
+    function wrapper() {
+        const self = this;
+        const elapsed = Number(new Date()) - lastExec;
+        const args = arguments;
+
+        function exec() {
+            lastExec = Number(new Date());
+            callback.apply(self, args);
+        }
+
+        clearTimeout(timeoutID);
+
+        if (elapsed > delay) {
+            exec();
+        } else {
+            timeoutID = setTimeout(exec, delay - elapsed);
+        }
+    }
+
+    return wrapper;
+}
+
+
 /* 滚动到页面顶部 */
 export const backTop=()=>{
     let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
